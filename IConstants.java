0fee8a4ad6ad67103663ae832d7be33b29b089d6
DaniPorras02 <daniporras97@gmail.com>
2017-06-17 10:46:59 -0600

    upgrade

--- IConstants.java
+++ IConstants.java
@@ -3,12 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mycar;
+package ejemplo;
 
-/**
- *
- * @author Daniela
- */
+
 public interface IConstants {
+    
+    public final float FIRST_GEAR_MAX_RPS = (float) 7.17;
+    
+    public final float SECOND_GEAR_MAX_RPS = (float) 11.2;
+    
+    public final float THIRD_GEAR_MAX_RPS = (float) 16.24;
+    
+    public final float FOURTH_GEAR_MAX_RPS = (float) 21.655;
+    
+    public final float FIFTH_GEAR_MAX_RPS = (float) 27.72;
+            
+    public final int MAX_GEAR = 5;
+    
+    public final int MIN_GEAR = 1;
     
 }
--- Motor.java
+++ Motor.java
@@ -3,12 +3,72 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mycar;
+package ejemplo;
 
-/**
- *
- * @author Daniela
- */
+import static ejemplo.IConstants.*;
+
 public class Motor {
+    
+    public float RPS;
+    public float AddingRPS;
+    public int ActualGear;
+    
+    public Motor(){
+        RPS = 0;
+        AddingRPS = FIRST_GEAR_MAX_RPS;
+        ActualGear = MIN_GEAR;
+    }
+    
+    public void increaseRPS(){
+        RPS+= AddingRPS;
+    }
+    
+    public void advanceGear(){
+        if (ActualGear < MAX_GEAR){
+            ActualGear++;
+            switch(ActualGear){
+                case 1:
+                    AddingRPS = FIRST_GEAR_MAX_RPS;
+                    break;
+                case 2:
+                    AddingRPS = SECOND_GEAR_MAX_RPS;
+                    break;
+                case 3:
+                    AddingRPS = THIRD_GEAR_MAX_RPS;
+                    break;
+                case 4:
+                    AddingRPS = FOURTH_GEAR_MAX_RPS;
+                    break;
+                case 5:
+                    AddingRPS = FIFTH_GEAR_MAX_RPS;
+                    break;
+                    
+            }
+        }
+    }
+    
+    public void decreaseGear(){
+        if (ActualGear > MIN_GEAR){
+            ActualGear--;
+            switch(ActualGear){
+                case 1:
+                    AddingRPS = FIRST_GEAR_MAX_RPS;
+                    break;
+                case 2:
+                    AddingRPS = SECOND_GEAR_MAX_RPS;
+                    break;
+                case 3:
+                    AddingRPS = THIRD_GEAR_MAX_RPS;
+                    break;
+                case 4:
+                    AddingRPS = FOURTH_GEAR_MAX_RPS;
+                    break;
+                case 5:
+                    AddingRPS = FIFTH_GEAR_MAX_RPS;
+                    break;
+                    
+            }
+        }
+    }
     
 }
